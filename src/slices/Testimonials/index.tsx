9234ae6ed248134b92f81348e5c24d551ac386c6
OdeliaFink <odelia.finkel@gmail.com>
2023-10-20 12:23:10 -0400

    added styling testimonial

--- src/slices/Testimonials/index.tsx
+++ src/slices/Testimonials/index.tsx
@@ -1,5 +1,26 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import Bounded from '@/components/Bounded';
+import { Content, isFilled } from '@prismicio/client';
+import {
+  PrismicRichText,
+  SliceComponentProps,
+  JSXMapSerializer,
+} from '@prismicio/react';
+import Heading from '@/components/Heading';
+import { createClient } from '@/prismicio';
+import { PrismicNextImage } from '@prismicio/next';
+
+const components: JSXMapSerializer = {
+  heading2: ({ children }) => (
+    <Heading as="h2" size="md" className="text-center mb-9 font-display">
+      {children}
+    </Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="text-xl md:text-2xl font-normal font-body text-slate-600 mb-8">
+      {children}
+    </p>
+  ),
+};
 
 /**
  * Props for `Testimonials`.
@@ -9,15 +30,55 @@ export type TestimonialsProps = SliceComponentProps<Content.TestimonialsSlice>;
 /**
  * Component for "Testimonials" Slices.
  */
-const Testimonials = ({ slice }: TestimonialsProps): JSX.Element => {
+const Testimonials = async ({
+  slice,
+}: TestimonialsProps): Promise<JSX.Element> => {
+  const client = createClient();
+  const testimonials = await Promise.all(
+    slice.items.map((item, index) => {
+      if (
+        isFilled.contentRelationship(item.testimonial) &&
+        item.testimonial.uid
+      ) {
+        return client.getByUID('testimonial', item.testimonial.uid);
+      }
+    })
+  );
+
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for testimonials (variation: {slice.variation})
-      Slices
-    </section>
+      <PrismicRichText field={slice.primary.heading} components={components} />
+      <div className="grid lg:grid-cols-3 grid-cols-1 gap-8">
+        {testimonials.map((item, index) => (
+          <div
+            className="border bg-white shadow-ld rounded-lg px-8 md:px-14 py-10 md:py-16 grid content-between"
+            key={index}
+          >
+            <PrismicRichText field={item?.data.quote} components={components} />
+            <div className="flex items-center">
+              <PrismicNextImage
+                width={56}
+                height={56}
+                field={item?.data.avatar}
+                className="rounded-full mr-4"
+                imgixParams={{ ar: '1:1', fit: 'crop' }}
+              />
+              <div>
+                <p className="text-base font-medium text-slate-700">
+                  {item?.data.name}
+                </p>
+                <p className="text-base text-slate-600">
+                  {item?.data.job_title}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Bounded>
   );
 };
 
